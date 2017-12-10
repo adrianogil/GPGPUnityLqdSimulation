9e774407ccbcb84fa40f118cb7dc26949cf4a2d0
Adriano Gil <adrianogil.san@gmail.com>
2017-12-10 10:14:52 -0400

    WIP Input

---
+++ Assets/Scripts/LiquidSimulation.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class LiquidSimulation : MonoBehaviour {
+
+    public RenderTexture liquidState;
+    public int stateSizeX = 200, stateSizeY = 200;
+
+	[Header("Color States")]
+    public Color blockColor;
+    public Color lightWaterColor;
+    public Color darkWaterColor;
+	public Color backgroundColor;
+
+
+    public Material GPGPUMaterial;
+    public Material GPGPUInputMaterial;
+    private MeshCollider collider;
+
+	// Use this for initialization
+	void Start () {
+		InitState();
+
+        collider = GetComponent<MeshCollider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			VerifyInputArea (1f);
+		} else if (Input.GetMouseButton (1)) {
+			VerifyInputArea (0f);
+		}
+	}
+
+	public void VerifyInputArea(float inputType)
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (collider.Raycast(ray, out hit, 100.0F)) {
+			//				Debug.Log(hit.point);
+			Vector2 uv = GetUVFromHitPoint(hit.point);
+			Debug.Log("UV: (" + uv.x + "," + uv.y + ")");
+
+			GPGPUInputMaterial.SetVector("_NewPos", uv);
+			GPGPUInputMaterial.SetFloat ("_InputType", inputType);
+			GPGPUInputMaterial.SetColor ("_BlockColor", blockColor);
+			GPGPUInputMaterial.SetColor("_LightWaterColor", lightWaterColor);
+			GPGPUInputMaterial.SetColor("_DarkWaterColor", darkWaterColor);
+            
+
+			Graphics.Blit (liquidState, liquidState, GPGPUInputMaterial);
+		}
+	}
+
+    public void InitState()
+    {
+		liquidState = new RenderTexture (stateSizeX, stateSizeY, 16, RenderTextureFormat.ARGB32);
+
+		Texture2D initStateTexture = new Texture2D (stateSizeX, stateSizeY);
+
+        for (int x = 0; x < stateSizeX; x++)
+        {
+            for (int y = 0; y < stateSizeY; y++)
+            {
+                if (x == 0 || x == stateSizeX - 1 || y == 0 || y == stateSizeY - 1)
+                {
+					initStateTexture.SetPixel(x,y, blockColor);
+                }
+                else {
+                    initStateTexture.SetPixel(x,y, backgroundColor);   
+                }
+            }
+        }
+		initStateTexture.Apply();
+
+		Graphics.Blit (initStateTexture, liquidState);
+
+		MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+		if (meshRenderer != null) {
+			meshRenderer.material.mainTexture = liquidState;
+		}
+    }
+
+    public Vector2 GetUVFromHitPoint(Vector3 point)
+    {
+        Vector2 uv;
+
+        Vector3 centerPosition = transform.position;
+		Vector3 size = transform.localScale;
+
+		Vector3 initialPosition = centerPosition - 0.5f * size;
+		Vector3 relativePosition = point - initialPosition;
+
+		uv.x = relativePosition.x / size.x;
+		uv.y = relativePosition.y / size.y;
+
+        return uv;
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(LiquidSimulation))]
+public class LiquidSimulationEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+    
+        LiquidSimulation editorObj = target as LiquidSimulation;
+    
+        if (editorObj == null) return;
+
+        if (GUILayout.Button("Initialize"))
+        {
+            editorObj.InitState();
+        }        
+    }
+
+}
+#endif
+
---
+++ Assets/Shaders/LiquidInput.shader
@@ -0,0 +1,74 @@
+﻿Shader "Custom/LiquInput"
+{
+    Properties
+    {
+        _BlockColor("Block Color", Color) = (1,1,1,1)
+        _LightWaterColor("Light Water Color", Color) = (1,1,1,1)
+        _DarkWaterColor("Dark Water Color", Color) = (1,1,1,1)
+        _NewPos ("New Position", Vector) = (0.5,0.5,0,0)
+        _InputType ("Input Type", float) = (0.5,0.5,0,0)
+        _MainTex("Liquid State", 2D) = "white"
+    }
+    Subshader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            sampler _MainTex;
+            float4 _MainTex_TexelSize;
+
+            float _InputType;
+
+            float2 _NewPos;
+            float4 _BlockColor;
+            float4 _LightWaterColor;
+            float4 _DarkWaterColor;
+
+            struct vert_input
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct vert_output
+            {
+                float4 vertex : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            vert_output vert(vert_input i)
+            {
+                vert_output o;
+
+                o.vertex = UnityObjectToClipPos(i.vertex);
+                o.uv = i.uv;
+
+                return o;
+            }
+
+            half4 frag(vert_output o) : COLOR
+            {
+                if (_InputType == 1)
+                {
+                    if (length(o.uv - _NewPos) < 0.025)
+                    {
+                        return _LightWaterColor;
+                    }
+                } else if (_InputType == 0)
+                {
+                    if (length(o.uv - _NewPos) < _MainTex_TexelSize.x)
+                    {
+                        return _BlockColor;
+                    }
+                }
+
+                return tex2D(_MainTex, o.uv);
+            }
+
+            ENDCG
+        }
+    }
+}
